28eccac2f237fb0fcbab0123bd264881ed57f925
trainer19 <trainer19@trainer19s-MacBook-Pro.local>
2017-03-30 11:49:32 -0600

    initial commit

---
+++ src/main/java/com/example/PagesController.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by trainer19 on 3/30/17.
+ */
+@RestController
+public class PagesController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
