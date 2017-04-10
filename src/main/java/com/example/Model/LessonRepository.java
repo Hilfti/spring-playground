1d0a340eedd1053934ab189fc639cf9918040e15
Hilfti <trainer19@trainer19s-MacBook-Pro.local>
2017-04-09 19:38:09 -0600

    Database to Spring

---
+++ src/main/java/com/example/Model/LessonRepository.java
@@ -0,0 +1,11 @@
+package com.example.Model;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by trainer19 on 4/9/17.
+ */
+public interface LessonRepository extends CrudRepository<Lesson, Long> {
+
+
+}
